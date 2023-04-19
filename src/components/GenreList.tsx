26e57dd9138dced877d230a8d5f4fb7bc5edf459
Edwin van den Bos <evandenbos@apro.nl>
2023-04-19 16:22:23 +0200

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 import UseGenres, { Genre } from "../hooks/UseGenres";
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-urls";
 
 const GenreList = () => {
   const { data } = UseGenres();
   return (
-    <ul>{data && data.map((genre) => <li key={genre.id}>{genre.name}</li>)}</ul>
+    <List>
+      {data &&
+        data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              ></Image>
+              <Text fontSize={"lg"}>{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+    </List>
   );
 };
 
