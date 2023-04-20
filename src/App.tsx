4b9e024320a90df3c29ee4da538db6f188fca80a
evdbos <81522191+evdbos@users.noreply.github.com>
2023-04-20 20:34:07 +0200

    filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/UseGenres";
 import PlatformSelector from "./components/PlatfomSelector";
+import { Platform } from "./hooks/usePlatform";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelectedPlatform] = useState<null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -33,8 +36,14 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        />
       </GridItem>
     </Grid>
   );
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,11 +1,7 @@
 import useData from "./UseData";
 import { Genre } from "./UseGenres";
+import { Platform } from "./usePlatform";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 export interface Game {
   id: number;
   name: string;
@@ -14,9 +10,12 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", [selectedGenre?.id], {
-    params: { genres: selectedGenre?.id },
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>("/games", [selectedGenre?.id, selectedPlatform?.id], {
+    params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id },
   });
 
 export default useGames;
