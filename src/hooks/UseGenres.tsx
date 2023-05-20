d3cd9b9cd3e3804d746fbead16b1a7b0be766c8b
evdbos <81522191+evdbos@users.noreply.github.com>
2023-05-20 14:37:08 +0200

    add reat query as hook for platform and genre

--- src/hooks/UseGenres.tsx
+++ src/hooks/UseGenres.tsx
@@ -1,11 +1,21 @@
 // import useData from "./UseData";
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./UseData";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-const UseGenres = () => ({ data: genres, isLoading: false, error: null }); //useData<Genre>("/genres");
+const UseGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default UseGenres;
