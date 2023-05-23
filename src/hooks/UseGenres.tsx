ed4f1200c148a7ad81ceb66403f8055c7df1f471
Edwin van den Bos <evandenbos@apro.nl>
2023-05-23 18:59:45 +0200

    refactoring create reusable api client

--- src/hooks/UseGenres.tsx
+++ src/hooks/UseGenres.tsx
@@ -1,7 +1,9 @@
 // import useData from "./UseData";
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import apiClient, { FetchResponse } from "../services/api-client";
+import ApiClient, { FetchResponse } from "../services/api-client";
+
+const apiClient = new ApiClient<Genre>("/genres");
 
 export interface Genre {
   id: number;
@@ -11,8 +13,7 @@ export interface Genre {
 const UseGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
     initialData: { count: genres.length, results: genres },
   });
