2638d221b5a0ef42b1cf04b339a1a1166adc1588
evdbos <81522191+evdbos@users.noreply.github.com>
2023-05-31 17:12:05 +0200

    add definition item

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Grid, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -15,6 +17,26 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <SimpleGrid columns={2}>
+        <DefinitionItem term="platforms">
+          {game.parent_platforms?.map(({ platform }) => (
+            <Text key={platform.id}>{platform.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="metascore">
+          <CriticScore score={game.metacritic}></CriticScore>
+        </DefinitionItem>
+        <DefinitionItem term="Genres">
+          {game.genres.map((genre) => (
+            <Text key={genre.id}>{genre.name}</Text>
+          ))}
+        </DefinitionItem>
+        <DefinitionItem term="Publishers">
+          {game.publishers?.map((publisher) => (
+            <Text key={publisher.id}>{publisher.name}</Text>
+          ))}
+        </DefinitionItem>
+      </SimpleGrid>
     </>
   );
 };
