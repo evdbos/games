c9ae2336c88e4c3efaaf46bece3e2f2e89897758
evdbos <81522191+evdbos@users.noreply.github.com>
2023-05-31 18:55:10 +0200

    refactor gameAttributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import ExpandableText from "../components/ExpandableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
 import GameTrailer from "../components/GameTrailer";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -18,27 +19,8 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-      <SimpleGrid columns={2}>
-        <DefinitionItem term="platforms">
-          {game.parent_platforms?.map(({ platform }) => (
-            <Text key={platform.id}>{platform.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="metascore">
-          <CriticScore score={game.metacritic}></CriticScore>
-        </DefinitionItem>
-        <DefinitionItem term="Genres">
-          {game.genres.map((genre) => (
-            <Text key={genre.id}>{genre.name}</Text>
-          ))}
-        </DefinitionItem>
-        <DefinitionItem term="Publishers">
-          {game.publishers?.map((publisher) => (
-            <Text key={publisher.id}>{publisher.name}</Text>
-          ))}
-        </DefinitionItem>
-      </SimpleGrid>
-      <GameTrailer gameId={game.id}></GameTrailer>
+      <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
